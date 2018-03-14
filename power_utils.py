6c03671b3551ab9e5b55479224f7425bfb46cc7c
u215200 <luis.mesen@united.com>
2018-03-14 18:48:43 -0500

    final

--- power_utils.py
+++ power_utils.py
@@ -72,12 +72,11 @@ def import_data():
 
 
 def create_train_set():
-    """This function performs feature engineering and further data cleansing and produces a train and test set
-    ::param:: dataset
-    the input data set to be prepared for modeling
+    """This function performs feature engineering and further data cleansing and produces a fully
+    pre-processed training set
 
-    ::return:: train, test
-    two Pandas DataFrame objects with train and test set splits
+    ::return:: train
+    a Pandas DataFrame object for analytical modeling
     """
     train, test = import_data()
 
@@ -137,21 +136,31 @@ def absolute_difference(obs_val, pred_val):
     return np.absolute(pred_val - obs_val)
 
 
-def score_predictions(train_loc, test_loc, prediction_loc, prediction_colname, prediction_cutoff, output_note):
-    train = pd.read_csv(train_loc)
+def score_predictions(train_loc, test_loc, prediction_loc, actual_colname,
+                      prediction_colname, prediction_cutoff, output_note):
+    train = pd.read_csv(train_loc, dtype={'meter_id': 'object', 'site_id': 'object'})
     predictions = pd.read_csv(prediction_loc)
     test = pd.read_csv(test_loc)
 
     train['predictions'] = predictions[prediction_colname]
 
     train_clean = train.drop_duplicates(['timestamp', 'meter_id'])
+
+    # Left join the test subset with the train superset
     combined = pd.merge(train_clean, test, how='left', left_on=['timestamp', 'meter_id'],
                         right_on=['Timestamp', 'meter_id'])
-    combined['abs_err'] = combined.apply(lambda r: absolute_difference(r['values'], r['predictions']), axis=1)
+
+    # Calculate absolute error for each 'values' prediction
+    combined['abs_err'] = combined.apply(lambda r: absolute_difference(r[actual_colname], r['predictions']), axis=1)
+    # Calculate the average within meter_id group of absolute errors
     combined = pd.merge(combined, combined.groupby('meter_id')['abs_err'].mean().reset_index(name='average_abs_err'),
                         how='left', on='meter_id')
+    # Calculate the anomaly score ratio of absolute error over the average absolute error (within meter_id group)
     combined['anomaly_score'] = combined['abs_err'] / combined['average_abs_err']
+
+    # What should the cutoff be?? experiment with quantile cutoffs
     anomaly_cutoff = combined['anomaly_score'].quantile(q=prediction_cutoff)
+    print('Anomaly cutoff: {}'.format(anomaly_cutoff))
     test_final = combined.dropna(subset=['is_abnormal'])
     test_final['is_abnormal'] = test_final.apply(lambda r: r['anomaly_score'] > anomaly_cutoff,
                                                  axis=1)
@@ -166,5 +175,72 @@ def score_predictions(train_loc, test_loc, prediction_loc, prediction_colname, p
                        index=False)
 
 
-def score_clusters():
-    pass
+def score_clusters(train_loc, test_loc, prediction_loc, prediction_colname, output_note):
+    train = pd.read_csv(train_loc, dtype={'meter_id': 'object', 'site_id': 'object'})
+    predictions = pd.read_csv(prediction_loc)
+    test = pd.read_csv(test_loc)
+
+    train['predictions'] = predictions[prediction_colname]
+
+    train_clean = train.drop_duplicates(['timestamp', 'meter_id'])
+    combined = pd.merge(train_clean, test, how='left', left_on=['timestamp', 'meter_id'],
+                        right_on=['Timestamp', 'meter_id'])
+
+    test_final = combined.dropna(subset=['is_abnormal'])
+
+    test_final['is_abnormal'] = test_final.apply(lambda r: r['predictions'] == 0 or r['predictions'] == 4 or
+                                                 r['predictions'] == 7 or r['predictions'] == 9, axis=1)
+
+    print(test_final['is_abnormal'].value_counts())
+
+    test_scored = test_final[['obs_id_y', 'meter_id', 'Timestamp', 'is_abnormal']]
+    test_scored.columns = ['obs_id', 'meter_id', 'Timestamp', 'is_abnormal']
+    test_scored['obs_id'] = test_scored.loc[:, 'obs_id'].astype(int)
+    now = datetime.now()
+    test_scored.to_csv('./output/test_scored_{}_{}.csv'.format(now.strftime("%y%m%d%H%M%S"), output_note),
+                       index=False)
+
+
+def score_novel_clusters(train_loc, test_loc, prediction_loc, cluster_loc, cluster_thresh, actual_colname,
+                         prediction_colname, prediction_cutoff, output_note):
+    train = pd.read_csv(train_loc, dtype={'meter_id': 'object', 'site_id': 'object'})
+    predictions = pd.read_csv(prediction_loc)
+    clusters = pd.read_csv(cluster_loc)
+    test = pd.read_csv(test_loc)
+
+    train['predictions'] = predictions[prediction_colname]
+
+    train_clean = train.drop_duplicates(['timestamp', 'meter_id'])
+
+    # Left join the test subset with the train superset
+    combined = pd.merge(train_clean, test, how='left', left_on=['timestamp', 'meter_id'],
+                        right_on=['Timestamp', 'meter_id'])
+
+    combined = pd.merge(combined, clusters, how='left', 'left_on')
+
+    # Calculate absolute error for each 'values' prediction
+    combined['abs_err'] = combined.apply(lambda r: absolute_difference(r[actual_colname], r['predictions']), axis=1)
+    # Calculate the average within meter_id group of absolute errors
+    combined = pd.merge(combined, combined.groupby('meter_id')['abs_err'].mean().reset_index(name='average_abs_err'),
+                        how='left', on='meter_id')
+    # Calculate the anomaly score ratio of absolute error over the average absolute error (within meter_id group)
+    combined['anomaly_score'] = combined['abs_err'] / combined['average_abs_err']
+
+    # What should the cutoff be?? experiment with quantile cutoffs
+    anomaly_cutoff = combined['anomaly_score'].quantile(q=prediction_cutoff)
+    print('Anomaly cutoff: {}'.format(anomaly_cutoff))
+    test_final = combined.dropna(subset=['is_abnormal'])
+    test_final['is_abnormal'] = test_final.apply(lambda r: r['anomaly_score'] > anomaly_cutoff,
+                                                 axis=1)
+
+    print('Cluster threshold: {}'.format(cluster_thresh))
+    test_final['is_abnormal']
+
+    print(test_final.is_abnormal.value_counts())
+
+    test_scored = test_final[['obs_id_y', 'meter_id', 'Timestamp', 'is_abnormal']]
+    test_scored.columns = ['obs_id', 'meter_id', 'Timestamp', 'is_abnormal']
+    test_scored['obs_id'] = test_scored.loc[:, 'obs_id'].astype(int)
+    now = datetime.now()
+    test_scored.to_csv('./output/test_scored_{}{}.csv'.format(now.strftime("%y%m%d%H%M%S"), output_note),
+                       index=False)
